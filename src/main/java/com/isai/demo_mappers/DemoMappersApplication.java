bf83262cde688a28084c51764dc8e5d65b33426b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-18 17:19:11 -0500

    init

---
+++ src/main/java/com/isai/demo_mappers/DemoMappersApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_mappers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMappersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMappersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_mappers/DemoMappersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_mappers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMappersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
